b9df7bd676d78978a0800a326fb8f0c39c4b12d5
Banyar Po Po <40524300@live.napier.ac.uk>
2022-12-13 11:24:05 +0630

    Added Employee javaclass

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -79,4 +79,31 @@ public class App
             }
         }
     }
+    public com.napier.sem.Employee getEmployee(int ID) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next()) {
+                com.napier.sem.Employee emp = new com.napier.sem.Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            } else
+                return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
 }
